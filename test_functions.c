7071bd60594e016411d1199bba9ae2817387004a
Raul Viana <raulmanuelviana@gmail.com>
2020-10-06 00:19:40 +0100

    testing opening files

--- test_functions.c
+++ test_functions.c
@@ -1,9 +1,41 @@
 
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h> 
+#include <unistd.h>
+#include <unistd.h>
+
+
 
 
 int main(){
 
+    struct stat meta;
+
+    int fd_file = open("pinguim.gif", O_RDONLY);
+
+    fstat(fd_file, &meta);
+    
+    char buf[meta.st_size];
+
+    read(fd_file, &buf, sizeof(buf));
+
+    printf("FileSIze: %ld\n", meta.st_size);
+
+    printf("Buf size: %ld\n", sizeof(buf));
+
+    for(int i = 0; i < sizeof(buf); i++){
+        printf("%d", buf[i]);
+    }
+   
+
+
+    
+    close(fd_file);
 
     return 0;
 }
