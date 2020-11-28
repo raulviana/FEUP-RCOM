9afde1f55a846c82b682a0e6787e2bc3e6cd0d14
Raul Viana <raulmanuelviana@gmail.com>
2020-11-28 17:24:03 +0000

    download app almost done

--- trabalho2/src/main.c
+++ trabalho2/src/main.c
@@ -32,9 +32,12 @@ int getip(char host[], url* url)
 	return 0;
 }
 
-int ftp_send_command(int socket_id, const char* command, int command_size);
+int ftp_send_command(int socket_id, const char* command, int command_size, char* response);
 int open_socket(const char* ip_address, const int port);
+int read_socket(int socket_id, char* response);
+int ftp_passive_mode(int socket_id, char* command, size_t command_size, char* response);
 
+int new_socket_id;
 
 int main(int argc, char** argv){
 
@@ -47,6 +50,9 @@ int main(int argc, char** argv){
 	struct	sockaddr_in server_addr;
 	char buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";  
 	int	bytes;
+	
+	char command[MAX_COMMAND_LENGTH];
+	char response[MAX_STRING_LENGTH];
 
     url url;
 
@@ -76,25 +82,40 @@ int main(int argc, char** argv){
 		exit(2);
 	}
 
+    printf("\n");
+	read_socket(socket_id, response);
+
 	printf("FTP connection established\n");
 
 	/* Log In in ftp server */
-	char command[MAX_COMMAND_LENGTH];
+	
 	//user
-	sprintf(command, "user %s", url.user);
-	if(ftp_send_command(socket_id, command, strlen(command))){
+	sprintf(command, "USER %s\r\n", url.user);
+	if(ftp_send_command(socket_id, command, strlen(command), response)){
 		perror("Error sending command USER\n");
 		exit(3);
 	}
 	//pass
 	bzero(command, MAX_COMMAND_LENGTH);
-	sprintf(command, "pass %s", url.password);
-	if(ftp_send_command(socket_id, command, strlen(command))){
+	bzero(response, MAX_STRING_LENGTH);
+	sprintf(command, "PASS %s\r\n", url.password);
+	
+	if(ftp_send_command(socket_id, command, strlen(command), response)){
 		perror("Error sending command PASSWORD\n");
 		exit(4);
 	}
 
+	//enter passive mode
+	bzero(command, MAX_COMMAND_LENGTH);
+	bzero(response, MAX_STRING_LENGTH);
+	sprintf(command, "PASV\r\n", url.password);
+	if (ftp_passive_mode(socket_id, command, strlen(command), response)) {
+		perror("Error in ftp_passive_mode()\n");
+		exit(5);
+    }
+    printf("Entered passive mode successfully\n");
 
+	printf("new_socket: %d\n", new_socket_id);
 
 }
 
@@ -123,12 +144,66 @@ int open_socket(const char* ip_address, const int port){
 	return socket_fd;
 }
 
-int ftp_send_command(int socket_id, const char* command, int command_size){
+int ftp_send_command(int socket_id, const char* command, int command_size, char* response){
 	int res = write(socket_id, command, command_size);
 	if( res <= 0){
 		perror("Error in ftp_send_command()\n");
 		return TRUE;
 	}
 	printf("Written %d bytes: %s\n", res, command);
+	
+	printf("Server response: \n");
+	if(read_socket(socket_id, response)){
+		perror("Error reading from socket\n");
+		return TRUE;
+	};
+	printf("\n");
 	return FALSE;
+}
+
+int read_socket(int socket_id, char* response){
+	FILE* fp = fdopen(socket_id, "r");
+	do{
+		memset(response, 0, MAX_STRING_LENGTH);
+		response = fgets(response, MAX_STRING_LENGTH, fp);
+		printf("%s", response);
+	}  while (!('1' <= response[0] && response[0] <= '5') || response[3] != ' ');
+	// response[3] == ' ' means received a last status line
+	//response [0] <= 5 means received numerated status line
+	printf("\n\n");
+	return 0;
+}
+
+int ftp_passive_mode(int socket_id, char* command, size_t command_size, char* response){
+	if(ftp_send_command(socket_id, command, command_size, response)){
+		perror("Error in ftp_send_command()");
+		exit(4);
+	}
+	
+	char ip_address[MAX_STRING_LENGTH];
+	int port_num;
+	int ip1, ip2, ip3, ip4;
+	int port1, port2;
+
+	//copy values from response to corresponding variables
+	if (sscanf(response, "227 Entering Passive Mode (%d,%d,%d,%d,%d,%d)", &ip1, &ip2, &ip3, &ip4, &port1, &port2) < 0) {
+		perror("sscanf()\n");
+		return 1;
+	}
+
+	// Creating server ip address
+	sprintf(ip_address, "%d.%d.%d.%d", ip1, ip2, ip3, ip4);
+
+	// Calculating tcp port number
+	port_num = port1 * 256 + port2;
+
+	printf("IP: %s\n", ip_address);
+	printf("PORT: %d\n", port_num);
+
+	if ((new_socket_id = open_socket(ip_address, port_num)) < 0) {
+		perror("open_socket()\n");
+		return 1;
+	}
+
+	return 0;
 }
